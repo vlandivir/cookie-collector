027028c330d997ac8ecddef4a63374513e11e966
Vladimir Rybakov <vladimir.rybakov@gmail.com>
2024-08-06 17:17:44 +0200

    refactoring

--- specs/example.spec.ts
+++ specs/example.spec.ts
@@ -1,79 +1,15 @@
-import { test, expect, Page } from '@playwright/test';
+import { test } from '@playwright/test';
 import dotenv from 'dotenv';
-declare global {
-    interface Window {
-        clientSideCookies: {
-            url: string;
-            cookieString: string;
-        }[];
-    }
-}
 
-interface CookieRequest {
-    url: string;
-    cookies: {
-        name: string;
-        cookie: string;
-    }[];
-    type: 'client' | 'server';
-}
+import { addCookieCollector, parseClientSideCookies, getCollectedCookies } from '../tools/cookies-collector';
 
 dotenv.config();
-const requests: CookieRequest[] = [];
 
-function parseCookies(cookieString: string) {
-    return cookieString.split('\n').map(cookie => {
-        let [cookiePair, ...attributePairs] = cookie.split(';').map(part => part.trim());
-        let [name] = cookiePair.split('=');
-
-        const attribs: Record<string, string> = {}
-        attributePairs.forEach(attr => {
-            let [key, val] = attr.split('=').map(part => part.trim());
-            attribs[key.toLowerCase()] = val || 'true';
-        });
-        return {name: [attribs['domain'], name].join(' ').trim(), cookie};
-    });
-}
-
-const getUrl = (url: string) => {
-    const parsedUrl = new URL(url);
-    return [parsedUrl.host, parsedUrl.pathname.split(':').shift()].join('');
-}
-
-const addCookieCollector = (page: Page) => {
-    page.on('response', async response => {
-        const request = response.request();
-
-        const url = getUrl(request.url());
-        const setCookieHeader = (await response.allHeaders())['set-cookie'];
-
-        if (setCookieHeader) {
-            const cookies = parseCookies(setCookieHeader)
-            requests.push({ url, cookies, type: 'server' });
-        }
-    });
-}
 
 // Example test suite
 test.describe('Example Test Suite', () => {
     test.afterAll(async () => {
-        // console.log(JSON.stringify(requests, null, 2));
-
-        const collectedCookies: Record<string, {type: string, urls: string[]}> = {};
-        requests.forEach((r) => {
-            const { url, cookies, type } = r;
-            cookies.forEach(c => {
-                const { name } = c;
-                if (!collectedCookies[name]) {
-                    collectedCookies[name] = { type, urls: [] }
-                }
-                if (!collectedCookies[name].urls.includes(url)) {
-                    collectedCookies[name].urls.push(url);
-                }
-            })
-        });
-
-        // console.log(JSON.stringify(collectedCookies, null, 2))
+        const collectedCookies = getCollectedCookies();
         console.log(JSON.stringify(
             Object.keys(collectedCookies).map((k) => {
                 const { type, urls } = collectedCookies[k];
@@ -85,47 +21,10 @@ test.describe('Example Test Suite', () => {
     // Example test case
     test('should navigate to the page and check title', async ({ page }) => {
         // Collect all requests and their responses
-        addCookieCollector(page);
-
-        await page.addInitScript(() => {
-            // Save the original document.cookie property descriptor
-            const originalCookieDescriptor: any = Object.getOwnPropertyDescriptor(Document.prototype, 'cookie');
-            window.clientSideCookies = [];
-
-            // Define a new property descriptor for document.cookie
-            Object.defineProperty(document, 'cookie', {
-                configurable: true,
-                enumerable: true,
-                get() {
-                    // Return the current cookies
-                    return originalCookieDescriptor.get.call(document);
-                },
-                set(cookieString) {
-                    // Capture the stack trace to identify the source
-                    try {
-                        throw new Error('Tracking cookie set');
-                    } catch (e: any) {
-                        const matches = String(e.stack).match(/\((http[^)]+)\)/)
-                        if (matches) {
-                            const url = matches[1];
-                            console.log('Cookie url', url);
-                            window.clientSideCookies.push({ url, cookieString })
-                        }
-                    }
-
-                    // Set the cookie using the original descriptor's setter
-                    originalCookieDescriptor.set.call(document, cookieString);
-                }
-            });
-        });
+        await addCookieCollector(page);
 
         // Navigate to the page
         await page.goto('https://tripleten.com/sso/auth?retpath=%2Fprofile%2F%3Ffrom%3D%252Fsuccess%252F');
-
-        // const [loginPage] = await Promise.all([
-        //     page.context().waitForEvent('page'),
-        //     page.locator('[id="header"] .header__login-button').click()
-        // ]);
 
         await page.fill('#username', process.env.USERMAIL || '');
         await page.locator(".input_type_submit").click();
@@ -133,17 +32,12 @@ test.describe('Example Test Suite', () => {
         await page.fill('#password', process.env.PASSWORD || '');
         await page.locator(".input_type_submit").click();
 
-        await page.goto('https://tripleten.com/');
+        await page.goto('https://tripleten.com/se-light/');
 
         // Wait for 40 seconds
         await page.waitForTimeout(40 * 1000);
 
-        const clientSideCookies = await page.evaluate(() => window.clientSideCookies);
-        clientSideCookies.forEach((cookie) => {
-            const {url, cookieString} = cookie;
-            const cookies = parseCookies(cookieString)
-            requests.push({ url: getUrl(url), cookies, type: 'client' });
-        });
+        await parseClientSideCookies(page);
 
         // Check if the heading text is correct
         // const heading = page.locator('h1');
---
+++ tools/cookies-collector.ts
@@ -0,0 +1,111 @@
+import { Page } from '@playwright/test';
+
+declare global {
+    interface Window {
+        clientSideCookies: {
+            url: string;
+            cookieString: string;
+        }[];
+    }
+}
+
+interface CookieRequest {
+    url: string;
+    cookies: {
+        name: string;
+        cookie: string;
+    }[];
+    type: 'client' | 'server';
+}
+
+const requests: CookieRequest[] = [];
+
+const parseCookies = (cookieString: string) => {
+    return cookieString.split('\n').map(cookie => {
+        let [cookiePair, ...attributePairs] = cookie.split(';').map(part => part.trim());
+        let [name] = cookiePair.split('=');
+
+        const attribs: Record<string, string> = {}
+        attributePairs.forEach(attr => {
+            let [key, val] = attr.split('=').map(part => part.trim());
+            attribs[key.toLowerCase()] = val || 'true';
+        });
+        return {name: [attribs['domain'], name].join(' ').trim(), cookie};
+    });
+}
+
+const getUrl = (url: string) => {
+    const parsedUrl = new URL(url);
+    return [parsedUrl.host, parsedUrl.pathname.split(':').shift()].join('');
+}
+
+export const addCookieCollector = async (page: Page) => {
+    page.on('response', async response => {
+        const request = response.request();
+
+        const url = getUrl(request.url());
+        const setCookieHeader = (await response.allHeaders())['set-cookie'];
+
+        if (setCookieHeader) {
+            const cookies = parseCookies(setCookieHeader)
+            requests.push({ url, cookies, type: 'server' });
+        }
+    });
+
+    await page.addInitScript(() => {
+        // Save the original document.cookie property descriptor
+        const originalCookieDescriptor: any = Object.getOwnPropertyDescriptor(Document.prototype, 'cookie');
+        window.clientSideCookies = [];
+
+        // Define a new property descriptor for document.cookie
+        Object.defineProperty(document, 'cookie', {
+            configurable: true,
+            enumerable: true,
+            get() {
+                // Return the current cookies
+                return originalCookieDescriptor.get.call(document);
+            },
+            set(cookieString) {
+                // Capture the stack trace to identify the source
+                try {
+                    throw new Error('Tracking cookie set');
+                } catch (e: any) {
+                    const matches = String(e.stack).match(/\((http[^)]+)\)/)
+                    if (matches) {
+                        const url = matches[1];
+                        window.clientSideCookies.push({ url, cookieString })
+                    }
+                }
+
+                // Set the cookie using the original descriptor's setter
+                originalCookieDescriptor.set.call(document, cookieString);
+            }
+        });
+    });
+}
+
+export const parseClientSideCookies = async (page: Page) => {
+    const clientSideCookies = await page.evaluate(() => window.clientSideCookies);
+    clientSideCookies.forEach((cookie) => {
+        const {url, cookieString} = cookie;
+        const cookies = parseCookies(cookieString)
+        requests.push({ url: getUrl(url), cookies, type: 'client' });
+    });
+}
+
+export const getCollectedCookies = () => {
+    const collectedCookies: Record<string, {type: string, urls: string[]}> = {};
+    requests.forEach((r) => {
+        const { url, cookies, type } = r;
+        cookies.forEach(c => {
+            const { name } = c;
+            if (!collectedCookies[name]) {
+                collectedCookies[name] = { type, urls: [] }
+            }
+            if (!collectedCookies[name].urls.includes(url)) {
+                collectedCookies[name].urls.push(url);
+            }
+        })
+    });
+    return collectedCookies;
+}
